9e0a2d785bac262791acdb5a673214cf737811a5
Ariya <rastpourariya@gmail.com>
2026-08-20 19:44:22 +0330

    Website templated
    Add  website templates => ['about','contact','index']

---
+++ core/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'accounts'
+
+# urlpatterns = [
+#     path("signup/", views.SignUpView.as_view(), name="sign-up"),
+#     path("login/", views.LoginView.as_view(), name="login"),
+#     path("logout/", views.LogoutView.as_view(), name="logout"),
+# ]
--- core/blog/urls.py
+++
@@ -1,8 +0,0 @@
-# blog urls
-from django.urls import path
-from django.shortcuts import render
-from . import views
-
-urlpatterns = [
-    path('',views.IndexView.as_view())
-]
