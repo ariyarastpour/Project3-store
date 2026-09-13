5cd2b96d4b6645091aab3bb35b31c4887ffe2141
Ariya <rastpourariya@gmail.com>
2026-09-13 17:22:48 +0330

    To correct project

--- core/shop/api/v1/serializers.py
+++ core/shop/api/v1/serializers.py
@@ -1,11 +1,7 @@
 from rest_framework import serializers
 from ...models import Product, Category
-<<<<<<< Updated upstream
-from django.urls import reverse
-=======
 from rest_framework.reverse import reverse
 from accounts.models import Profile
->>>>>>> Stashed changes
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,18 +12,12 @@ class CategorySerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     discounted_price = serializers.ReadOnlyField(source="offer")
     relative_url = serializers.ReadOnlyField(source="get_absolute_url")
-<<<<<<< Updated upstream
-    absolute_url = serializers.SerializerMethodField(
-        method_name="obj_absolute_url"
-    )
-=======
     absolute_url = serializers.SerializerMethodField(method_name="obj_absolute_url")
->>>>>>> Stashed changes
+
 
     class Meta:
         model = Product
         fields = [
-<<<<<<< Updated upstream
             "id",
             'user',
             "category",
@@ -54,33 +44,6 @@ class ProductSerializer(serializers.ModelSerializer):
             "updated_date",
         ]
 
-    
-    def obj_absolute_url(self, obj):
-        request = self.context.get("request")
-        return request.build_absolute_uri(
-            reverse("shop:shop-api-v1:products-detail", kwargs={"pk": obj.pk})
-        )
-
-    def to_representation(self, instance):
-        rep = super().to_representation(instance)
-        request = self.context.get("request")
-        if request.parser_context.get("kwargs").get("pk"):
-            rep.pop("relative_url")
-            rep.pop("absolute_url")
-        rep["category"] = CategorySerializer(instance.category).data
-        return rep
-    
-    def create(self, validated_data):
-        validated_data['user'] = self.context['request'].user
-        return super().create(validated_data)
-=======
-            "id", "user", "category", "title", "slug", "image", "description",
-            "stock", "status", "price", "discount_percent", "discounted_price",
-            "sells", "avg_rate", "relative_url", "absolute_url",
-            "created_date", "updated_date",
-        ]
-        read_only_fields = ["user", "sells", "avg_rate", "created_date", "updated_date"]
->>>>>>> Stashed changes
 
     def obj_absolute_url(self, obj):
         request = self.context.get("request")
